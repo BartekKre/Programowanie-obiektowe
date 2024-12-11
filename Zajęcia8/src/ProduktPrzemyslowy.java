e42c76c8fa307525b21d66b58a3e7a78c3971969
BartekKre <147046159+BartekKre@users.noreply.github.com>
2024-12-11 17:48:31 +0100

    Delete Zajęcia8/src directory

--- Zajęcia8/src/ProduktPrzemyslowy.java
+++
@@ -1,5 +0,0 @@
-public class ProduktPrzemyslowy extends Produkt{
-    public ProduktPrzemyslowy(String nazwa, double cena, int iloscNaMagazynie) {
-        super(nazwa, cena, iloscNaMagazynie);
-    }
-}
--- Zajęcia8/src/Ubranie.java
+++
@@ -1,7 +0,0 @@
-public abstract class Ubranie {
-    public abstract void wypierz();
-    public abstract void zaloz();
-    public abstract void wyprasuj();
-    public abstract void wysusz();
-    public abstract void zniszcz();
-}
